0697d87034bee8aec036dec2bf56f705c10b42c7
lemoji06 <michaelgacheru006@gmail.com>
2026-06-27 15:24:22 +0300

    for loops

---
+++ forloop.js
@@ -0,0 +1,13 @@
+for (let i=0; i<5; i++){
+    console.log(i);
+}
+for (let i=10; i>0; i--){
+    console.log(i);
+
+}
+for (let i=0; i<10; i+=2){
+    console.log(i);
+}
+for (let i=0; i<=30; i+=5){
+    console.log(i);
+}
