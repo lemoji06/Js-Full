c06b1df26a9129ac1d85eade5a3d7948006cc4cb
lemoji06 <michaelgacheru006@gmail.com>
2026-06-27 14:50:37 +0300

    if else condition

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ let age = 20;
 if (age=>18){
     console.log("You are an adult.");
 }
+else{
+    console.log("You are a minor.");
+}
 
 console.log("My name is " + name );
 console.log("I am " + age + " years old.");
---
+++ marks.js
@@ -0,0 +1,11 @@
+let marks =75;
+if (marks>=70){
+     console.log("Distinction");
+}
+else if ((marks>=50) && (marks<=69)){
+    console.log("pass");
+}
+else if(marks<50){
+    console.log("fail");
+}
+
